bb4658dc145925f7f7d94fb8a4ee30fba5d5294e
pete <work@pete-hamlin.com>
2025-10-20 22:06:31 +0100

    fix: Fix hooks

--- hooks/on-add.gtd.py
+++ hooks/on-add.gtd.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-import sys
 import json
 import subprocess
+import sys
 
 task = json.loads(sys.stdin.readline())
 
 project = task.get("project")
 msg = ""
 
-if 'project' in task:
-    msg = subprocess.run(['gtd','add',project], check=True, stdout=subprocess.PIPE, universal_newlines=True)
+if "project" in task:
+    msg = subprocess.run(["proj", "add", project], check=True, stdout=subprocess.PIPE, universal_newlines=True)
 
 print(json.dumps(task))
 if msg:
